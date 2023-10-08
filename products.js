e2d8c968545dab63e2d008f8ed8519edcdfcf20d
Samiksha W <73897765+Samiksha1603@users.noreply.github.com>
2023-10-09 00:33:56 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,43 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  123: {
+    name : "HP Headsets",
+    img : "11.jfif",
+    price : 1500
+  },
+  124: {
+    name : "Lenovo Headsets",
+    img : "12.jfif",
+    price : 1600
+  },
+  125: {
+    name : "Dell Headsets",
+    img : "13.jfif",
+    price : 1700
+  },
+  126: {
+    name : "Sony Headsets",
+    img : "14.jfif",
+    price : 1800
+  },
+  127: {
+    name : "Jabra Headsets",
+    img : "15.jfif",
+    price : 1900
+  },
+  128: {
+    name : "SkullCandy Headsets",
+    img : "16.jfif",
+    price : 2000
+  },
+  129: {
+    name : "Zebronics Headsets",
+    img : "17.jfif",
+    price : 123
+  },
+  130: {
+    name : "iPon Min",
+    img : "18.jfif",
+    price : 123
+  },
+};
